ae0988d03a7946185ea9879b852ba5ac23209f8c
Roman Tsukanov <roman@tsukanov.me>
2014-09-26 15:17:49 +1000

    Added basic Flask stuff.

---
+++ run.py
@@ -0,0 +1,8 @@
+﻿from werkzeug.serving import run_simple
+from metabrainz import create_app
+
+application = create_app()
+
+if __name__ == '__main__':
+    run_simple('0.0.0.0', 5000, application,
+               use_reloader=True, use_debugger=True, use_evalex=True)
