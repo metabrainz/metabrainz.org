696894153a2d8511fcaaf13077447bfb7736da24
Roman Tsukanov <roman@tsukanov.me>
2014-09-26 16:23:13 +1000

    Added configuration files.

--- metabrainz/__init__.py
+++ metabrainz/__init__.py
@@ -4,6 +4,11 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
 
+    # Configuration
+    import default_config
+    app.config.from_object(default_config)
+    app.config.from_object('metabrainz.config')
+
     @app.route("/")
     def index():
         return render_template('index.html')
