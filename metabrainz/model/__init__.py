117887a331c5e863c46f638b0cbc0129815dfc6b
Roman Tsukanov <roman@tsukanov.me>
2014-09-29 22:53:02 +1000

    Throwing away unnecessary tables.

--- metabrainz/model/__init__.py
+++ metabrainz/model/__init__.py
@@ -4,7 +4,3 @@ db = SQLAlchemy()
 
 # All models must be imported there:
 from .donation import Donation
-from .donation_historical import DonationHistorical
-from .pending import Pending
-from .pending_data import PendingData
-from .replication_control import ReplicationControl
