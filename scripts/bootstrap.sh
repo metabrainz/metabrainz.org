2b7783a9dea78b345cf9b3dfa916af396f19767a
Roman Tsukanov <roman@tsukanov.me>
2014-10-01 00:31:03 +1000

    Added tools for Less compilation. Added Fabric.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python-virtualenv python-dev git
+apt-get install -y build-essential python-virtualenv python-dev git curl
+
+# Less compiler
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y nodejs
+npm install -g less
 
 cd /vagrant
 pip install -r requirements.txt
