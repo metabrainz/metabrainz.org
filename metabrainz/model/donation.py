9a52cd4fd314e8a54258350b9d9ec54ac0f656dd
Roman Tsukanov <roman@tsukanov.me>
2014-10-02 01:02:16 +1000

    Implemented creation of donation records in the database.

--- metabrainz/model/donation.py
+++ metabrainz/model/donation.py
@@ -5,7 +5,7 @@ import re
 
 
 class Donation(db.Model):
-    __tablename__ = "donation"
+    __tablename__ = 'donation'
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -13,21 +13,21 @@ class Donation(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    moderator = db.Column(db.String, server_default="")
-    contact = db.Column(db.Boolean, server_default="FALSE")
-    anonymous = db.Column("anon", db.Boolean, server_default="FALSE")
-    address_street = db.Column(db.String, server_default="")
-    address_city = db.Column(db.String, server_default="")
-    address_state = db.Column(db.String, server_default="")
-    address_postcode = db.Column(db.String, server_default="")
-    address_country = db.Column(db.String, server_default="")
+    moderator = db.Column(db.String, server_default='')
+    contact = db.Column('contact', db.Boolean, server_default='FALSE')
+    anonymous = db.Column('anon', db.Boolean, server_default='FALSE')
+    address_street = db.Column(db.String, server_default='')
+    address_city = db.Column(db.String, server_default='')
+    address_state = db.Column(db.String, server_default='')
+    address_postcode = db.Column(db.String, server_default='')
+    address_country = db.Column(db.String, server_default='')
 
     # Transaction details
-    timestamp = db.Column(db.DateTime, server_default="now()")
+    timestamp = db.Column(db.DateTime, server_default='now()')
     paypal_trans_id = db.Column(db.String(32), nullable=False)
     amount = db.Column(db.Numeric(11, 2), nullable=False)
-    fee = db.Column(db.String, server_default="")
-    memo = db.Column(db.String, server_default="")
+    fee = db.Column(db.String, server_default='')
+    memo = db.Column(db.String, server_default='')
 
     def verify_and_log_wepay_checkout(self, checkout_id, editor, anonymous, can_contact):
         # Looking up updated information about the object
@@ -44,9 +44,34 @@ class Donation(db.Model):
 
         if details['state'] in ['settled', 'captured']:
             # Payment has been received
-            # TODO: Create a record in the database.
+
+            new_donation = Donation(
+                first_name=details['payer_name'],
+                last_name='',
+                email=details['payer_email'],
+                moderator=editor,
+                can_contact=can_contact,
+                anonymous=anonymous,
+                amount=details['gross']-details['fee'],
+                fee=details['fee'],
+            )
+
+            if 'shipping_address' in details:
+                address = details['shipping_address']
+                new_donation.address_street = "%s\n%s" % (address['address1'], address['address2'])
+                new_donation.address_city = address['city']
+                if 'state' in address:  # US address
+                    new_donation.address_state = address['state']
+                else:
+                    new_donation.address_state = address['region']
+                if 'zip' in address:  # US address
+                    new_donation.address_postcode = address['zip']
+                else:
+                    new_donation.address_postcode = address['postcode']
+
+            db.session.add(new_donation)
+            db.session.commit()
             # TODO: Send receipt.
-            pass
 
         elif details['state'] in ['authorized', 'reserved']:
             # Payment is pending
