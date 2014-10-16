7bea1f7d4e336b0ab1a8fecce8bea3a0a250981f
Roman Tsukanov <roman@tsukanov.me>
2014-10-17 07:06:40 +1000

    Removed default config file.

--- metabrainz/__init__.py
+++ metabrainz/__init__.py
@@ -5,8 +5,6 @@ def create_app():
     app = Flask(__name__)
 
     # Configuration
-    import default_config
-    app.config.from_object(default_config)
     app.config.from_object('metabrainz.config')
 
     # Database
