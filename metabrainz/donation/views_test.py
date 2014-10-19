15a843598c2c1cbc60e1697908b88869eb0432ee
Roman Tsukanov <roman@tsukanov.me>
2014-10-19 21:16:29 +1000

    Updated tests for donation views (again).

--- metabrainz/donation/views_test.py
+++ metabrainz/donation/views_test.py
@@ -4,25 +4,20 @@ from metabrainz.testing import FlaskTestCase
 class DonationViewsTestCase(FlaskTestCase):
 
     def test_index(self):
-        response = self.client.get("/donate/")
-        self.assert200(response)
+        self.assert200(self.client.get("/donate/"))
 
     def test_paypal(self):
-        response = self.client.get("/donate/paypal")
-        self.assert200(response)
+        self.assert200(self.client.get("/donate/paypal"))
 
     def test_wepay(self):
-        response = self.client.get("/donate/wepay")
-        self.assert200(response)
+        self.assert200(self.client.get("/donate/wepay"))
 
     def test_complete(self):
         self.assert200(self.client.get("/donate/complete"))
         self.assert200(self.client.post("/donate/complete"))
 
     def test_cancelled(self):
-        response = self.client.get("/donate/cancelled")
-        self.assert200(response)
+        self.assert200(self.client.get("/donate/cancelled"))
 
     def test_error(self):
-        response = self.client.get("/donate/error")
-        self.assert200(response)
+        self.assert200(self.client.get("/donate/error"))
