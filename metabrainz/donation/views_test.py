5ff527121ffb7bedb9a37c4e76036948632ba4f9
Roman Tsukanov <roman@tsukanov.me>
2014-10-19 21:14:41 +1000

    Updated tests for donation views.

--- metabrainz/donation/views_test.py
+++ metabrainz/donation/views_test.py
@@ -16,7 +16,11 @@ class DonationViewsTestCase(FlaskTestCase):
         self.assert200(response)
 
     def test_complete(self):
-        response = self.client.get("/donate/complete")
+        self.assert200(self.client.get("/donate/complete"))
+        self.assert200(self.client.post("/donate/complete"))
+
+    def test_cancelled(self):
+        response = self.client.get("/donate/cancelled")
         self.assert200(response)
 
     def test_error(self):
