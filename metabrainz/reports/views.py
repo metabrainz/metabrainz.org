54ae644fead6fb5111ad10d40a310d88c2526f75
Roman Tsukanov <roman@tsukanov.me>
2014-10-28 20:54:49 -0700

    Added 2013 annual report.

--- metabrainz/reports/views.py
+++ metabrainz/reports/views.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+from jinja2.exceptions import TemplateNotFound
 
 reports_bp = Blueprint('reports', __name__)
 
 
-@reports_bp.route('/')
-def index():
-    # TODO: Create this page.
-    return "Page is missing."
+@reports_bp.route('/<year>')
+def index(year):
+    """This endpoint handles requests for pages with annual reports.
+    
+    If you want to create a new report just add a new HTML file into
+    /metabrainz/templates/reports directory.
+    """
+    try:
+        return render_template('reports/%s.html' % year)
+    except TemplateNotFound:
+        return NotFound("Requested annual report was not found.")
