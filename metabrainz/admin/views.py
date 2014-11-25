f7694485136b01f2d9623fac14bd167c5d44c6c9
Roman Tsukanov <roman@tsukanov.me>
2014-11-26 02:23:40 +0900

    Added receipt sending for manual addition form.

--- metabrainz/admin/views.py
+++ metabrainz/admin/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, flash
 from metabrainz.decorators import requires_auth
 from metabrainz.model.donation import Donation
 from metabrainz.admin.forms import AddDonationForm
+from metabrainz.donations.receipts import send_receipt
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -12,7 +13,7 @@ def add_donation():
     form = AddDonationForm()
 
     if form.validate_on_submit():
-        donation = Donation.add_donation(
+        new_donation = Donation.add_donation(
             first_name=form.first_name.data,
             last_name=form.last_name.data,
             email=form.email.data,
@@ -29,7 +30,14 @@ def add_donation():
             anonymous=form.anonymous.data,
         )
         flash('Donation has been added. (ID: %s, Amount: %s, Fee: %s)'
-              % (donation.id, donation.amount, donation.fee),
+              % (new_donation.id, new_donation.amount, new_donation.fee),
               'success')
+        send_receipt(
+            new_donation.email,
+            new_donation.timestamp,
+            new_donation.amount,
+            '%s %s' % (new_donation.first_name, new_donation.last_name),
+            new_donation.moderator,
+        )
 
     return render_template('admin/add_donation.html', form=form)
