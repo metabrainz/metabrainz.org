581f247ac1fdafe6a9995940a7235470a4d12905
Roman Tsukanov <roman@tsukanov.me>
2014-12-10 08:14:36 +0900

    Combined views related to donations.

--- metabrainz/donations/views.py
+++ metabrainz/donations/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from __future__ import division
+from flask import Blueprint, request, render_template, url_for, redirect
 from metabrainz.model.donation import Donation
+from math import ceil
 
 donations_bp = Blueprint('donations', __name__)
 
@@ -8,6 +10,40 @@ donations_bp = Blueprint('donations', __name__)
 def index():
     """Home page for donations."""
     return render_template('donations/donate.html')
+
+
+@donations_bp.route('/donors')
+def donors():
+    page = int(request.args.get('page', default=1))
+    if page < 1:
+        return redirect(url_for('.donations'))
+    limit = 30
+    offset = (page - 1) * limit
+    count, donations = Donation.get_recent_donations(limit=limit, offset=offset)
+
+    last_page = int(ceil(count / limit))
+    if page > last_page:
+        return redirect(url_for('.donations', page=last_page))
+
+    return render_template('donations/donors.html', donations=donations,
+                           page=page, last_page=last_page)
+
+
+@donations_bp.route('/highest-donors')
+def highest_donors():
+    page = int(request.args.get('page', default=1))
+    if page < 1:
+        return redirect(url_for('.highest_donors'))
+    limit = 30
+    offset = (page - 1) * limit
+    count, donations = Donation.get_biggest_donations(limit=limit, offset=offset)
+
+    last_page = int(ceil(count / limit))
+    if page > last_page:
+        return redirect(url_for('.highest_donors', page=last_page))
+
+    return render_template('donations/donors_highest.html', donations=donations,
+                           page=page, last_page=last_page)
 
 
 @donations_bp.route('/nag-check/<editor>')
--- metabrainz/reports/financial_reports/views.py
+++ metabrainz/reports/financial_reports/views.py
@@ -1,7 +1,4 @@
-from __future__ import division
-from flask import Blueprint, request, render_template, url_for, redirect
-from metabrainz.model.donation import Donation
-from math import ceil
+from flask import Blueprint, render_template
 
 financial_reports_bp = Blueprint('financial_reports', __name__, static_folder='files')
 
@@ -9,37 +6,3 @@ financial_reports_bp = Blueprint('financial_reports', __name__, static_folder='f
 @financial_reports_bp.route('/')
 def index():
     return render_template('financial_reports/index.html')
-
-
-@financial_reports_bp.route('/donations')
-def donations():
-    page = int(request.args.get('page', default=1))
-    if page < 1:
-        return redirect(url_for('.donations'))
-    limit = 30
-    offset = (page - 1) * limit
-    count, donations = Donation.get_recent_donations(limit=limit, offset=offset)
-
-    last_page = int(ceil(count / limit))
-    if page > last_page:
-        return redirect(url_for('.donations', page=last_page))
-
-    return render_template('financial_reports/donations.html', donations=donations,
-                           page=page, last_page=last_page)
-
-
-@financial_reports_bp.route('/highest-donors')
-def highest_donors():
-    page = int(request.args.get('page', default=1))
-    if page < 1:
-        return redirect(url_for('.highest_donors'))
-    limit = 30
-    offset = (page - 1) * limit
-    count, donations = Donation.get_biggest_donations(limit=limit, offset=offset)
-
-    last_page = int(ceil(count / limit))
-    if page > last_page:
-        return redirect(url_for('.highest_donors', page=last_page))
-
-    return render_template('financial_reports/highest-donors.html', donations=donations,
-                           page=page, last_page=last_page)
