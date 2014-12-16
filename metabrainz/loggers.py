9bcc6f104a663f30da5fc454acbdc8ca03994b87
Roman Tsukanov <roman@tsukanov.me>
2014-12-16 15:50:03 +0900

    Fixed config access from email logger.

--- metabrainz/loggers.py
+++ metabrainz/loggers.py
@@ -4,15 +4,12 @@ from logging.handlers import SMTPHandler
 
 def add_email_handler(app):
     """Adds email notifications."""
-    credentials = None
-    if 'MAIL_USERNAME' in app.config or 'MAIL_PASSWORD' in app.config:
-        credentials = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
     mail_handler = SMTPHandler(
-        (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        app.config['MAIL_FROM_ADDR'],
+        (app.config['SMTP_SERVER'], app.config['SMTP_PORT']),
+        'no-reply@' + app.config['MAIL_FROM_DOMAIN'],
         app.config['LOG_RECIPIENTS'],
-        app.config['LOG_EMAIL_TOPIC'],
-        credentials)
+        app.config['LOG_EMAIL_TOPIC']
+    )
     mail_handler.setLevel(logging.ERROR)
 
     mail_handler.setFormatter(logging.Formatter('''
