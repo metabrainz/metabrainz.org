75b604e77e6d553cf0b3ee1da0cc9d0181b83a14
Roman Tsukanov <roman@tsukanov.me>
2014-12-18 12:30:19 +0900

    Updated tests.

--- metabrainz/views_test.py
+++ metabrainz/views_test.py
@@ -1,20 +1,21 @@
 from metabrainz.testing import FlaskTestCase
+from flask import url_for
 
 
 class IndexViewsTestCase(FlaskTestCase):
 
     def test_homepage(self):
-        response = self.client.get("/")
+        response = self.client.get(url_for('index.home'))
         self.assert200(response)
 
     def test_about(self):
-        response = self.client.get("/about")
+        response = self.client.get(url_for('index.about'))
         self.assert200(response)
 
     def test_sponsors(self):
-        response = self.client.get("/sponsors")
+        response = self.client.get(url_for('index.sponsors'))
         self.assert200(response)
 
     def test_privacy_policy(self):
-        response = self.client.get("/privacy")
+        response = self.client.get(url_for('index.privacy_policy'))
         self.assert200(response)
