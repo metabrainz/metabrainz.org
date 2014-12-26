63ef5d10dad547f26c1a8e1aebd22efe808b8fc3
Roman Tsukanov <roman@tsukanov.me>
2014-12-27 05:59:20 +0900

    Simplified code that starts application. Using Flask's function to do that.

--- run.py
+++ run.py
@@ -4,5 +4,4 @@ from metabrainz import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    run_simple('0.0.0.0', 8080, application,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+    application.run('0.0.0.0', 8080)
