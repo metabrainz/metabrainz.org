559088aeaff7f1a283a247ca54ebdac8e502e0f8
Roman Tsukanov <roman@tsukanov.me>
2015-05-07 21:02:19 +0900

    Added more commands into fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,18 @@
 from __future__ import with_statement
 from fabric.api import local
-from fabric.colors import green
-from fabric.colors import yellow
-from metabrainz import create_app
+from fabric.colors import yellow, green
+from metabrainz import create_app, cache
 from metabrainz.model.utils import init_postgres
+
+
+def git_pull():
+    local("git pull origin")
+    print(green("Updated local code.", bold=True))
+
+
+def install_requirements():
+    local("pip install -r requirements.txt")
+    print(green("Installed requirements.", bold=True))
 
 
 def compile_styling():
@@ -15,6 +24,19 @@ def compile_styling():
     style_path = "metabrainz/static/css/"
     local("lessc --clean-css %smain.less > %smain.css" % (style_path, style_path))
     print(green("Style sheets have been compiled successfully.", bold=True))
+
+
+def clear_memcached():
+    with create_app().app_context():
+        cache.flush_all()
+    print(green("Flushed everything from memcached.", bold=True))
+
+
+def deploy():
+    git_pull()
+    install_requirements()
+    compile_styling()
+    clear_memcached()
 
 
 def test(init_db=True, coverage=True):
