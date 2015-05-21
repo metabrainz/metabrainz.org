5a1b26f7ffeff0fac1dff2f100dc73a47c8702d4
Robert Kaye <rob@musicbrainz.org>
2015-05-21 18:54:42 +0200

    Added first cut of the replication check for nagios

--- metabrainz/api/views.py
+++ metabrainz/api/views.py
@@ -6,6 +6,7 @@ from metabrainz.api.decorators import token_required, tracked
 import logging
 import re
 import os
+import time
 
 api_bp = Blueprint('api', __name__)
 
@@ -17,11 +18,64 @@ MIMETYPE_SIGNATURE = 'text/plain'
 DAILY_SUBDIR = 'daily'
 WEEKLY_SUBDIR = 'weekly'
 
+# These durations are used to create nagios compatible status codes so we can monitor
+# the replication packet stream.
+MAX_PACKET_AGE_WARNING = 60 * 60 * 2 # 4 hours
+MAX_PACKET_AGE_CRITICAL = 60 * 60 * 6 # 4 hours
 
 @api_bp.route('/')
 def info():
     """This view provides information about using the API."""
     return render_template('api/info.html')
+
+@api_bp.route('/musicbrainz/replication-check')
+def replication_check():
+    """Check that all the replication packets are contiguous and that no packet is more than a 
+       few hours old. Output a Nagios compatible line of text."""
+
+    try:
+        entries = [os.path.join(current_app.config['REPLICATION_PACKETS_DIR'], e) 
+                     for e in os.listdir(current_app.config['REPLICATION_PACKETS_DIR'])]
+    except OSError as e:
+        logging.warning(e)
+        return Response("UNKNOWN " + str(e), mimetype='text/plain')
+
+    pattern = re.compile("replication-[0-9]+.tar.bz2$")
+    entries = filter(lambda x: pattern.search(x), entries)
+    entries = filter(os.path.isfile, entries)
+    entries.sort()
+    print entries
+
+    resp = "OK"
+    last = -1
+    pattern = re.compile("replication-([0-9]+).tar.bz2$")
+    for entry in entries:
+        print entry
+        m = pattern.search(entry)
+        if not m:
+            resp = "UNKNOWN Unkown files in the replication directory"
+            break
+        num = int(m.groups()[0])
+        print num
+        if last < 0:
+            last = num - 1
+
+        if last != num - 1:
+            resp = "CRITICAL Replication packet %d is missing" % (num -1)
+
+        last = num
+
+    print resp
+    if resp != "OK":
+        return Response(resp, mimetype='text/plain')
+        
+    last_packet_age = time.time() - os.path.getctime(entries[-1]) 
+    if last_packet_age > MAX_PACKET_AGE_CRITICAL:
+        resp = "CRITICAL Latest replication packet is %.1f hours old" % (latest_packet_page / 3600)
+    elif last_packet_age > MAX_PACKET_AGE_WARNING:
+        resp = "WARNING Latest replication packet is %.1f hours old" % (latest_packet_page / 3600)
+
+    return Response(resp, mimetype='text/plain')
 
 
 @api_bp.route('/musicbrainz/replication-info')
@@ -56,7 +110,6 @@ def replication_info():
             "replication-weekly-[0-9]+.tar.bz2$"
         ),
     })
-
 
 @api_bp.route('/musicbrainz/replication-<int:packet_number>.tar.bz2')
 @token_required
