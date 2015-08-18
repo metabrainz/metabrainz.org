c1cf2b52dd83fd2430eee0fd47abc37a36ab994a
Roman Tsukanov <roman@tsukanov.me>
2015-08-19 07:29:46 +0900

    Added schema update script.

---
+++ admin/schema_updates/8.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+CREATE TYPE token_log_action_types AS ENUM (
+  'deactivate',
+  'create'
+);
+
+CREATE TABLE token_log (
+  token       CHARACTER VARYING        NOT NULL,
+  "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL,
+  action      token_log_action_types   NOT NULL,
+  user_id     INTEGER                  NOT NULL,
+  CONSTRAINT token_log_pkey PRIMARY KEY (token, "timestamp", action),
+  CONSTRAINT token_log_token_fkey FOREIGN KEY (token)
+    REFERENCES token (value) MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT token_log_user_id_fkey FOREIGN KEY (user_id)
+    REFERENCES "user" (id) MATCH SIMPLE
+    ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+COMMIT;
