938b96d1708d593d1a1acfb1acd7d4ed165e76ac
Roman Tsukanov <roman@tsukanov.me>
2015-10-13 23:40:11 +0200

    Add new user state: limited.

--- metabrainz/admin/views.py
+++ metabrainz/admin/views.py
@@ -1,7 +1,7 @@
 from flask import Response
 from flask_admin import expose
 from metabrainz.admin import AdminIndexView, AdminBaseView
-from metabrainz.model.user import User, STATE_PENDING, STATE_ACTIVE, STATE_REJECTED, STATE_WAITING
+from metabrainz.model.user import User, STATE_PENDING, STATE_ACTIVE, STATE_REJECTED, STATE_WAITING, STATE_LIMITED
 from metabrainz.model.token import Token
 from metabrainz.model.token_log import TokenLog
 from metabrainz.model.access_log import AccessLog
@@ -53,8 +53,12 @@ class UsersView(AdminBaseView):
     @expose('/approve')
     def approve(self):
         user_id = request.args.get('user_id')
-        User.get(id=user_id).set_state(STATE_ACTIVE)
-        flash.info("User #%s has been approved." % user_id)
+        if request.args.get('limited'):
+            User.get(id=user_id).set_state(STATE_LIMITED)
+            flash.warn("User #%s has been approved (with limited access)." % user_id)
+        else:
+            User.get(id=user_id).set_state(STATE_ACTIVE)
+            flash.info("User #%s has been approved." % user_id)
 
         # Redirecting to the next pending user
         next_user = User.get(state=STATE_PENDING)
