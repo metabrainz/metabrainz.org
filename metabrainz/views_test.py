03b105b0850afb23ce7737846b74c44c3d1e43e1
Roman Tsukanov <roman@tsukanov.me>
2016-01-29 17:33:07 +0100

    Add tests for new pages.

--- metabrainz/views_test.py
+++ metabrainz/views_test.py
@@ -16,6 +16,14 @@ class IndexViewsTestCase(FlaskTestCase):
         response = self.client.get(url_for('index.about'))
         self.assert200(response)
 
+    def test_projects(self):
+        response = self.client.get(url_for('index.projects'))
+        self.assert200(response)
+
+    def test_team(self):
+        response = self.client.get(url_for('index.team'))
+        self.assert200(response)
+
     def test_sponsors(self):
         response = self.client.get(url_for('index.sponsors'))
         self.assert200(response)
