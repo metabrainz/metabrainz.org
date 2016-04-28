35b245c89c7e0d6641149dfeda00bc05f2e89025
Roman Tsukanov <roman@tsukanov.me>
2016-04-28 10:22:23 +0200

    Replace Flask-Script with click
    This also changes the way database is intialized by moving creation into SQL
    scripts. This makes it more explicit and easier to update.
    
    Version of PostgreSQL used in Vagrant is update to 9.4.

---
+++ metabrainz/db/__init__.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
+engine = None
+
+
+def init_db_engine(connect_str):
+    global engine
+    engine = create_engine(connect_str, poolclass=NullPool)
+
+
+def run_sql_script(sql_file_path):
+    with open(sql_file_path) as sql:
+        connection = engine.connect()
+        connection.execute(sql.read())
+        connection.close()
