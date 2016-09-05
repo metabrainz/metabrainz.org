2da5f865ea25c0980db2b913c4e3ca9748be3afe
Robert Kaye <rob@musicbrainz.org>
2016-09-05 19:20:44 +0200

    Adding reports for July and August

---
+++ misc/parse-stripe.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys, os
+import decimal
+import csv
+
+def toFloat(svalue):
+    return float(svalue.replace(",", ""))
+
+fp = None
+try:
+    fp = open(sys.argv[1], "r")
+except IOError:
+    print "Cannot open input file %s" % sys.argv[1]
+    exit(0)
+
+out = None
+try:
+    out = open(sys.argv[2], "w")
+    out.write("Date,Description,Amount\n")
+except IOError:
+    print "Cannot open output file %s" % sys.argv[2]
+    exit(0)
+
+trans = []
+reader = csv.reader(fp)
+for i, row in enumerate(reader):
+    if not i:
+        continue
+
+    if row[1] == 'transfer':
+        continue
+
+    print row
+    date = row[2].split(' ')[0]
+    date = date.split('-')
+    date = "%s/%s/%s" % (date[1], date[2], date[0])
+    sender = row[24]
+    amount = toFloat(row[6])
+    fee = -toFloat(row[8])
+    net = amount - fee
+    memo = row[1]
+
+    out.write("%s,%s,%.2f\n" % (date, "Stripe", fee))
+    out.write("%s,%s,%.2f\n" % (date, sender, amount))
+
+fp.close()
+out.close()
