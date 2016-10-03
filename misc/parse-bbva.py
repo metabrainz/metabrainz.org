e86aa102bb9e2dda6728c51e90d4089ad890291c
Robert Kaye <rob@musicbrainz.org>
2016-10-03 16:30:39 +0200

    Toss CX, add BBVA

---
+++ misc/parse-bbva.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+print "Date,Description,Amount"
+
+lines = sys.stdin.readlines()
+for line in lines:
+    stripped = line.strip()
+    if len(stripped) == 0 or not stripped[0].isdigit():
+        continue
+
+    fields = stripped.split('\t')
+
+    desc = fields[4]
+    if desc.startswith("'"):
+        desc = desc [1:]
+    if desc.endswith("'"):
+        desc = desc [:-1]
+    desc = re.sub("^\d\d\d\d\d\d\d\-", "", desc)
+    desc = re.sub("^\d\d\d\d\d\d\d\d\d\d ", "", desc)
+    desc = re.sub("^\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d ", "", desc)
+    desc = re.sub("  ", " ", desc)
+    desc = re.sub("  ", " ", desc)
+    desc = re.sub("  ", " ", desc)
+    desc = re.sub("  ", " ", desc)
+    dat = fields[0].split('/')
+    dat = "%s/%s/%s" % (dat[1], dat[0], dat[2])
+    amount = fields[6].replace(".", "")
+    amount = amount.replace(",", ".")
+    amount = float(amount)
+    print("%s,%s,%.2f" % (dat, desc, amount))
