cf178a9e05ffde8927a49f0c480355018055c637
Roman Tsukanov <roman@tsukanov.me>
2017-04-24 14:43:32 +0200

    Remove unused database creation code from manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from werkzeug.serving import run_simple
 from metabrainz import db
 from metabrainz import create_app
 from metabrainz.model.access_log import AccessLog
-from metabrainz.model.utils import init_postgres, create_tables as db_create_tables
 import subprocess
 import os
 import click
@@ -31,49 +30,20 @@ def runserver(host, port, debug=False):
 
 
 @cli.command()
-def create_db():
-    """Create and configure the database."""
-    with application.app_context():
-        init_postgres(application.config['SQLALCHEMY_DATABASE_URI'])
-
-@cli.command()
-@click.option("--host", "-h", default="0.0.0.0", show_default=True)
-@click.option("--port", "-p", default=8080, show_default=True)
-@click.option("--debug", "-d", is_flag=True,
-              help="Turns debugging mode on or off. If specified, overrides "
-                   "'DEBUG' value in the config file.")
-def runserver(host, port, debug):
-    create_app().run(host=host, port=port, debug=debug)
-
-@cli.command()
-def create_tables():
-    with application.app_context():
-        db_create_tables(application.config['SQLALCHEMY_DATABASE_URI'])
-
-def _run_psql(script, database=None):
-    script = os.path.join(ADMIN_SQL_DIR, script)
-    command = ['psql', '-p', str(application.config["PG_PORT"]), '-U', application.config["PG_SUPER_USER"], '-f', script]
-    if database:
-        command.extend(['-d', database])
-    return subprocess.call(command)
-
-@cli.command()
-def cleanup_logs():
-    with create_app().app_context():
-        AccessLog.remove_old_ip_addr_records()
-
-@cli.command()
 @click.option("--force", "-f", is_flag=True, help="Drop existing database and user.")
-def init_db(force):
+@click.option("--skip-create-db", "-s", is_flag=True,
+              help="Skip database creation step.")
+def init_db(force=False, skip_create_db=False):
     if force:
         exit_code = _run_psql('drop_db.sql')
         if exit_code != 0:
             raise Exception('Failed to drop existing database and user! Exit code: %i' % exit_code)
 
-    print('Creating user and a database...')
-    exit_code = _run_psql('create_db.sql')
-    if exit_code != 0:
-        raise Exception('Failed to create new database and user! Exit code: %i' % exit_code)
+    if not skip_create_db:
+        print('Creating user and a database...')
+        exit_code = _run_psql('create_db.sql')
+        if exit_code != 0:
+            raise Exception('Failed to create new database and user! Exit code: %i' % exit_code)
 
     print('Creating database extensions...')
     exit_code = _run_psql('create_extensions.sql', 'metabrainz')
@@ -147,6 +117,20 @@ def compile_translations():
     """Compile translations for use."""
     _run_command("pybabel compile -d metabrainz/translations")
     click.echo("Translated strings have been compiled and ready to be used.")
+
+
+@cli.command()
+def cleanup_logs():
+    with create_app().app_context():
+        AccessLog.remove_old_ip_addr_records()
+
+
+def _run_psql(script, database=None):
+    script = os.path.join(ADMIN_SQL_DIR, script)
+    command = ['psql', '-p', str(application.config["PG_PORT"]), '-U', application.config["PG_SUPER_USER"], '-f', script]
+    if database:
+        command.extend(['-d', database])
+    return subprocess.call(command)
 
 
 def _run_command(command):
