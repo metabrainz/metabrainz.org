2174c1a7bdd427e408dfa3bf9e0701bc63e84f62
Roman Tsukanov <roman@tsukanov.me>
2017-04-28 20:03:13 +0200

    MEB-91: Add support for customer payments in EUR

--- metabrainz/payments/views_test.py
+++ metabrainz/payments/views_test.py
@@ -7,8 +7,12 @@ class PaymentsViewsTestCase(FlaskTestCase):
     def test_donate(self):
         self.assert200(self.client.get(url_for('payments.donate')))
 
+    def test_payment_selector(self):
+        self.assert200(self.client.get(url_for('payments.payment_selector')))
+
     def test_payment(self):
-        self.assert200(self.client.get(url_for('payments.payment')))
+        self.assert200(self.client.get(url_for('payments.payment', currency='usd')))
+        self.assert200(self.client.get(url_for('payments.payment', currency='eur')))
 
     def test_cancel_recurring(self):
         self.assert200(self.client.get(url_for('payments.cancel_recurring')))
