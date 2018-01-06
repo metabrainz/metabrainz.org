9c38ff37c3db6f90545a155faf534e2d21fd9584
Robert Kaye <rob@musicbrainz.org>
2018-01-06 21:53:02 +0100

    Also fix up the paypal es script

--- misc/parse-paypal-es.py
+++ misc/parse-paypal-es.py
@@ -9,9 +9,28 @@ def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
     for row in csv_reader:
         yield [unicode(cell, 'utf-8') for cell in row]
 
-print "Date,Description,Amount"
+if len(sys.argv) != 3:
+    print "Usage parse-paypal-es.py <paypal csv file> <qbo csv file>"
+    sys.exit(-1)
 
-reader = unicode_csv_reader(sys.stdin)
+fp = None
+try:
+    fp = open(sys.argv[1], "r")
+except IOError:
+    print "Cannot open input file %s" % sys.argv[1]
+    sys.exit(0)
+
+out = None
+try:
+    out = open(sys.argv[2], "w")
+except IOError:
+    print "Cannot open output file %s" % sys.argv[2]
+    sys.exit(0)
+
+
+out.write("Date,Description,Amount\n")
+
+reader = unicode_csv_reader(fp)
 index = 0
 for fields in reader:
     if not index:
@@ -25,10 +44,13 @@ for fields in reader:
     if status != 'Completed':
         continue
 
-    print("%s,%s,%s" % (dat, desc, amount))
+    out.write("%s,%s,%s\n" % (dat, desc, amount))
 
     desc = "PayPal"
     dat = fields[0].encode('utf8')
     fee = fields[8].encode('utf8')
     if fee and float(fee) != 0.0:
-        print("%s,%s,%s" % (dat, desc, fee))
+        out.write("%s,%s,%s\n" % (dat, desc, fee))
+
+fp.close()
+out.close()
