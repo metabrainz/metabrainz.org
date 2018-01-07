cfd0a1b5ab2c80c22f3ecbbb08e19f8668613d9b
TheJYKoder <thejykoder@gmail.com>
2018-01-07 18:00:41 -0500

    Update utils.py

--- metabrainz/utils.py
+++ metabrainz/utils.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse, parse_qsl, urlunparse, urlencode
+import urllib.parse
 
 import random
 import string
@@ -29,3 +29,16 @@ def build_url(base, additional_params=None):
         (url.scheme, url.netloc, url.path, url.params,
          urlencode(query_params), url.fragment)
     )
+
+def parameterize(value, key):
+    """
+    Add a new parameter to the current url.
+    Taken from: http://stackoverflow.com/a/2506477
+    """
+    url_parts = list(urllib.parse.urlparse(request.url))
+
+    query = urllib.parse.parse_qs(url_parts[4])
+    query[key] = value
+    url_parts[4] = urllib.parse.urlencode(query, doseq=True)
+
+    return urllib.parse.urlunparse(url_parts)
