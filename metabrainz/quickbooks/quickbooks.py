ffe8972a1b512c70f047dffb41b3eaaff6c92297
Robert Kaye <rob@musicbrainz.org>
2018-07-16 16:28:28 +0200

    Update docs to reflect that custom_config is now gone.

--- metabrainz/quickbooks/quickbooks.py
+++ metabrainz/quickbooks/quickbooks.py
@@ -7,7 +7,7 @@ session_manager = None
 def init(app):
     global session_manager
     session_manager = Oauth2SessionManager(
-            sandbox=True,
+            sandbox=app.config["QUICKBOOKS_SANDBOX"],
             client_id=app.config["QUICKBOOKS_CLIENT_ID"],
             client_secret=app.config["QUICKBOOKS_CLIENT_SECRET"],
             base_url=app.config["QUICKBOOKS_CALLBACK_URL"],
