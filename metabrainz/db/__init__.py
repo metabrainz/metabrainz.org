73c009ee2189bf2d8826735f4ad7e206dffeb3ef
Kartik Ohri <kartikohri13@gmail.com>
2021-06-07 16:25:32 +0530

    Run all sql files using psycopg2

--- metabrainz/db/__init__.py
+++ metabrainz/db/__init__.py
@@ -1,7 +1,8 @@
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 
-engine = None
+engine: sqlalchemy.engine.Engine = None
 
 
 def init_db_engine(connect_str):
@@ -11,6 +12,23 @@ def init_db_engine(connect_str):
 
 def run_sql_script(sql_file_path):
     with open(sql_file_path) as sql:
-        connection = engine.connect()
-        connection.execute(sql.read())
-        connection.close()
+        with engine.connect() as connection:
+            connection.execute(sql.read())
+
+
+def run_sql_script_without_transaction(sql_file_path):
+    with open(sql_file_path) as sql:
+        connection = engine.connect().execution_options(isolation_level="AUTOCOMMIT")
+        lines = sql.read().splitlines()
+        try:
+            # If we try to execute multiple statements at once, postgres wraps them in a transaction.
+            # see https://www.psycopg.org/docs/usage.html#transactions-control for details.
+            for line in lines:
+                # TODO: Not a great way of removing comments. The alternative is to catch
+                # the exception sqlalchemy.exc.ProgrammingError "can't execute an empty query"
+                if line and not line.startswith("--"):
+                    connection.execute(line)
+        except sqlalchemy.exc.ProgrammingError as e:
+            print("Error: {}".format(e))
+        finally:
+            connection.close()
