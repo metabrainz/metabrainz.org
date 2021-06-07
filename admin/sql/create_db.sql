ebaa6cf9a79504688511aa12f5fd624e0215fb2d
Kartik Ohri <kartikohri13@gmail.com>
2021-06-07 16:25:02 +0530

    Alter sql file to make them run through psycopg2

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -1,6 +1,5 @@
-\set ON_ERROR_STOP 1
-
 -- Create the user and the database. Must run as user postgres.
 
 CREATE USER metabrainz NOCREATEDB NOSUPERUSER;
+ALTER USER metabrainz WITH PASSWORD 'metabrainz';
 CREATE DATABASE metabrainz WITH OWNER = metabrainz TEMPLATE template0 ENCODING = 'UNICODE';
--- admin/sql/drop_db.sql
+++ admin/sql/drop_db.sql
@@ -1,4 +1,2 @@
-\set ON_ERROR_STOP 1
-
 DROP DATABASE IF EXISTS metabrainz;
 DROP USER IF EXISTS metabrainz;
