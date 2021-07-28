6d144c7464df45b5a9db6c6fdc42aa951cfb3850
Kartik Ohri <kartikohri13@gmail.com>
2021-07-29 01:59:42 +0530

    Initial OAuth Work commit

---
+++ metabrainz/new_oauth/models/token.py
@@ -0,0 +1,19 @@
+import time
+
+from authlib.integrations.sqla_oauth2 import OAuth2TokenMixin
+
+from metabrainz.model import db
+
+
+class OAuth2Token(db.Model, OAuth2TokenMixin):
+    __tablename__ = 'oauth2_token'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
+
+    def is_refresh_token_active(self):
+        if self.revoked:
+            return False
+        expires_at = self.issued_at + self.expires_in * 2
+        return expires_at >= time.time()
