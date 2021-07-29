88f773add8bc5a8f8d6578fc733ef24f5859b3e9
Kartik Ohri <kartikohri13@gmail.com>
2021-07-29 18:49:29 +0530

    Add RefreshTokenGrant

---
+++ metabrainz/new_oauth/refresh_grant.py
@@ -0,0 +1,21 @@
+from authlib.oauth2.rfc6749 import grants
+
+from metabrainz.new_oauth.models import db
+from metabrainz.new_oauth.models.token import OAuth2Token
+from metabrainz.new_oauth.models.user import User
+
+
+class RefreshTokenGrant(grants.RefreshTokenGrant):
+
+    def authenticate_refresh_token(self, refresh_token):
+        token = OAuth2Token.query.filter_by(refresh_token=refresh_token).first()
+        if token and token.is_refresh_token_active():
+            return token
+
+    def authenticate_user(self, credential):
+        return User.query.get(credential.user_id)
+
+    def revoke_old_credential(self, credential):
+        credential.revoked = True
+        db.session.add(credential)
+        db.session.commit()
