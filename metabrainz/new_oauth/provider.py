2f78f173d35328a185173b6159914e32a3387cf1
Kartik Ohri <kartikohri13@gmail.com>
2021-07-29 17:26:48 +0530

    Add ImplicitGrant

--- metabrainz/new_oauth/provider.py
+++ metabrainz/new_oauth/provider.py
@@ -3,6 +3,7 @@ from authlib.integrations.sqla_oauth2 import (
     create_save_token_func
 )
 from authlib.integrations.flask_oauth2 import AuthorizationServer
+from authlib.oauth2.rfc6749 import ImplicitGrant
 from authlib.oauth2.rfc7636 import CodeChallenge
 
 from metabrainz.new_oauth.authorization_grant import AuthorizationCodeGrant
@@ -19,4 +20,5 @@ save_token = create_save_token_func(db.session, OAuth2Token)
 authorization_server = AuthorizationServer(query_client=query_client, save_token=save_token)
 
 authorization_server.register_grant(AuthorizationCodeGrant, [CodeChallenge(required=False)])
+authorization_server.register_grant(ImplicitGrant)
 
