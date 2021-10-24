bd8700f95755b0e407022f46984ec775dd1f2bb7
Kartik Ohri <kartikohri13@gmail.com>
2021-10-24 22:47:09 +0530

    Add explicit build image step during tests
    Otherwise, the user needs to manually ensure that the image is rebuilt
    after updating or installing new python dependencies etc. This is
    particularly problematic in CI where we cannot control the use of
    cached docker images.

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ function invoke_docker_compose {
 }
 
 invoke_docker_compose up -d db redis
+invoke_docker_compose build web
 invoke_docker_compose run --rm web \
     dockerize -wait tcp://db:5432 -timeout 60s \
     bash -c "python manage.py init_db --create-db && pytest --junitxml=reports/tests.xml"
