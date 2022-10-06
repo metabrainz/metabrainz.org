36a4c7188e24df03c652b35e34c2a4bd48bab036
Kartik Ohri <kartikohri13@gmail.com>
2022-10-06 18:53:50 +0530

    Update models

--- metabrainz/new_oauth/models/client.py
+++ metabrainz/new_oauth/models/client.py
@@ -1,13 +1,23 @@
-from metabrainz.new_oauth.models import db
-from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin
+from authlib.oauth2.rfc6749 import ClientMixin
+from sqlalchemy import Column, Text, ForeignKey, Integer, ARRAY, Identity
+from sqlalchemy.orm import relationship
 
 
-class OAuth2Client(db.Model, OAuth2ClientMixin):
+from metabrainz.new_oauth.models import Base
+
+
+class OAuth2Client(Base, ClientMixin):
+
     __tablename__ = 'oauth2_client'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, Identity(), primary_key=True)
+    client_id = Column(Text, nullable=False)
+    client_secret = Column(Text)
+    owner_id = Column(Integer, ForeignKey('oauth.user.id', ondelete='CASCADE'), nullable=False)
+    name = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    website = Column(Text)
+    redirect_uris = Column(ARRAY(Text), nullable=False)
 
-    # we probably want to do the migration in stages, so if the users are migrated
-    # at a different time than clients, we can temporarily drop the FK.
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User')
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'))
+    user = relationship('OAuth2User')
