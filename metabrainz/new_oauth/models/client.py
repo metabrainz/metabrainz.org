d5540b9d530c793269fbe126464763a7157c3772
Kartik Ohri <kartikohri13@gmail.com>
2022-10-06 18:53:50 +0530

    Update models

--- metabrainz/new_oauth/models/client.py
+++ metabrainz/new_oauth/models/client.py
@@ -1,16 +1,52 @@
-from metabrainz.new_oauth.models import db
-from authlib.integrations.sqla_oauth2 import OAuth2ClientMixin
+from authlib.oauth2.rfc6749 import ClientMixin
+from sqlalchemy import Column, Text, ForeignKey, Integer, ARRAY, Identity
+from sqlalchemy.orm import relationship
 
 
-class OAuth2Client(db.Model, OAuth2ClientMixin):
+from metabrainz.new_oauth.models import Base
+
+
+class OAuth2Client(Base, ClientMixin):
+
     __tablename__ = 'client'
     __table_args__ = {
         'schema': 'oauth'
     }
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = Column(Integer, Identity(), primary_key=True)
+    client_id = Column(Text, nullable=False)
+    client_secret = Column(Text)
+    owner_id = Column(Integer, ForeignKey('oauth.user.id', ondelete='CASCADE'), nullable=False)
+    name = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    website = Column(Text)
+    redirect_uris = Column(ARRAY(Text), nullable=False)
 
-    # we probably want to do the migration in stages, so if the users are migrated
-    # at a different time than clients, we can temporarily drop the FK.
-    user_id = db.Column(db.Integer, db.ForeignKey('oauth.user.id', ondelete='CASCADE'))
-    user = db.relationship('OAuth2User')
+    user = relationship('OAuth2User')
+
+    def get_client_id(self):
+        pass
+
+    def get_default_redirect_uri(self):
+        pass
+
+    def get_allowed_scope(self, scope):
+        pass
+
+    def check_redirect_uri(self, redirect_uri):
+        pass
+
+    def has_client_secret(self):
+        pass
+
+    def check_client_secret(self, client_secret):
+        pass
+
+    def check_token_endpoint_auth_method(self, method):
+        pass
+
+    def check_response_type(self, response_type):
+        pass
+
+    def check_grant_type(self, grant_type):
+        pass
--- metabrainz/new_oauth/models/code.py
+++ metabrainz/new_oauth/models/code.py
@@ -1,13 +1,38 @@
-from authlib.integrations.sqla_oauth2 import OAuth2AuthorizationCodeMixin
-from metabrainz.new_oauth.models import db
+from authlib.oauth2.rfc6749 import AuthorizationCodeMixin
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import Identity
+
+from metabrainz.new_oauth.models import Base
+from metabrainz.new_oauth.models.client import OAuth2Client
+from metabrainz.new_oauth.models.relation_scope import OAuth2CodeScope
+from metabrainz.new_oauth.models.user import OAuth2User
 
 
-class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
-    __tablename__ = 'code'
+class OAuth2AuthorizationCode(Base, AuthorizationCodeMixin):
+
+    __tablename__ = "code"
     __table_args__ = {
-        'schema': 'oauth'
+        "schema": "oauth"
     }
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('oauth.user.id', ondelete='CASCADE'))
-    user = db.relationship('OAuth2User')
+    id = Column(Integer, Identity(), primary_key=True)
+    user_id = Column(Integer, ForeignKey("oauth.user.id", ondelete="CASCADE"), nullable=False)
+    client_id = Column(Integer, ForeignKey("oauth.client.id", ondelete="CASCADE"), nullable=False)
+    code = Column(Text, nullable=False, unique=True)
+    redirect_uri = Column(Text, nullable=False)
+    response_type = Column(Text, nullable=False)
+    code_challenge = Column(Text)
+    code_challenge_method = Column(Text)
+    granted_at = Column(DateTime(timezone=True))
+
+    user = relationship(OAuth2User)
+    client = relationship(OAuth2Client)
+    scopes = relationship("OAuth2Scope", secondary=OAuth2CodeScope)
+
+    def get_redirect_uri(self):
+        pass
+
+    def get_scope(self):
+        pass
+
