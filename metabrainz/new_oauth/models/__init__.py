d688a331e60ccc28103b94b10bcebb48e1dee668
Kartik Ohri <kartikohri13@gmail.com>
2023-05-17 21:24:22 +0530

    fix ISEs and oauth setup

--- metabrainz/new_oauth/models/__init__.py
+++ metabrainz/new_oauth/models/__init__.py
@@ -1,5 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import declarative_base
-
-db = SQLAlchemy()
-Base = declarative_base()
+from .client import OAuth2Client
+from .code import OAuth2AuthorizationCode
+from .relation_scope import OAuth2TokenScope, OAuth2CodeScope
+from .scope import OAuth2Scope
+from .token import OAuth2Token
+from .user import OAuth2User
