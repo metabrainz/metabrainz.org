6da816bc235d49f81e3de9116bb88bffc74a7a2c
Kartik Ohri <kartikohri13@gmail.com>
2023-10-05 00:03:34 +0530

    Reactify user sign up, login, forgot/reset password/username forms

---
+++ frontend/js/src/forms/LostPassword.tsx
@@ -0,0 +1,103 @@
+import { Formik } from "formik";
+import React, { JSX } from "react";
+import { createRoot } from "react-dom/client";
+import * as Yup from "yup";
+import { getPageProps } from "../utils";
+import {
+  AuthCardContainer,
+  AuthCardTextInput,
+} from "./utils";
+
+type LostPasswordProps = {
+  recaptcha_site_key: string;
+  csrf_token: string;
+  initial_form_data: any;
+  initial_errors: any;
+};
+
+function LostPassword({
+  csrf_token,
+  recaptcha_site_key,
+  initial_form_data,
+  initial_errors,
+}: LostPasswordProps): JSX.Element {
+  return (
+    <AuthCardContainer>
+      <div className="auth-card-container">
+        <div className="auth-card">
+          <h1 className="page-title text-center">Forgot your password?</h1>
+          <Formik
+            initialValues={{
+              username: initial_form_data.username ?? "",
+              email: initial_form_data.email ?? "",
+              csrf_token,
+            }}
+            initialErrors={initial_errors}
+            initialTouched={initial_errors}
+            validationSchema={Yup.object({
+              username: Yup.string().required("Username is required!"),
+              email: Yup.string().email().required("Email is required!"),
+            })}
+            onSubmit={() => {}}
+          >
+            {({ errors, setFieldValue }) => (
+              <form method="POST">
+                <div className="form-group">
+                  <div className="col-sm-offset-4 col-sm-5">
+                    <input
+                      id="csrf_token"
+                      name="csrf_token"
+                      type="hidden"
+                      value={csrf_token}
+                    />
+                  </div>
+                  {errors.csrf_token && (
+                    <div className="alert alert-danger">
+                      {errors.csrf_token}
+                    </div>
+                  )}
+                </div>
+
+                <AuthCardTextInput
+                  label="Username"
+                  type="text"
+                  name="username"
+                  id="username"
+                  required
+                />
+
+                <AuthCardTextInput
+                  label="Email"
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                />
+
+        <button className="btn btn-primary main-action-button" type="submit">
+          Send Email
+        </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </AuthCardContainer>
+  );
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const { domContainer, reactProps, globalProps } = getPageProps();
+  const { recaptcha_site_key, csrf_token, initial_form_data, initial_errors } =
+    reactProps;
+
+  const renderRoot = createRoot(domContainer!);
+  renderRoot.render(
+    <LostPassword
+      recaptcha_site_key={recaptcha_site_key}
+      csrf_token={csrf_token}
+      initial_form_data={initial_form_data}
+      initial_errors={initial_errors}
+    />
+  );
+});
--- metabrainz/user/views.py
+++ metabrainz/user/views.py
@@ -1,7 +1,9 @@
+import json
 from datetime import datetime, timezone
 
 from flask import Blueprint, current_app, redirect, render_template, request, url_for
 from flask_login import logout_user, login_required, login_user, current_user
+from flask_wtf.csrf import generate_csrf
 
 from metabrainz import bcrypt, flash
 from metabrainz.model import db
@@ -23,26 +25,33 @@ def signup():
     if form.validate_on_submit():
         user = User.get(name=form.username.data)
         if user is not None:
-            form.form_errors.append(f"Another user with username '{form.username.data}' exists.")
-            return render_template("users/signup.html", form=form)
+            form.username.errors.append(f"Another user with username '{form.username.data}' exists.")
+        else:
+            # TODO: Handle the case where multiple users sign up with same email but haven"t verified it yet
+            user = User.get(email=form.email.data)
+            if user is not None:
+                form.email.errors.append(f"Another user with email '{form.email.data}' exists.")
+            else:
+                user = User.add(name=form.username.data, email=form.email.data, password=form.password.data)
+                send_verification_email(
+                    user,
+                    "Please verify your email address",
+                    "email/user-email-address-verification.txt"
+                )
+                login_user(user)
+                flash.success("Account created. Please check your inbox to complete verification.")
+                return redirect(url_for("index.home"))
 
-        # TODO: Handle the case where multiple users sign up with same email but haven"t verified it yet
-        user = User.get(email=form.email.data)
-        if user is not None:
-            form.form_errors.append(f"Another user with email '{form.email.data}' exists.")
-            return render_template("users/signup.html", form=form)
+    form_errors = {k: ". ".join(v) for k, v in form.errors.items()}
+    form_data = dict(**form.data)
+    form_data.pop("csrf_token", None)
 
-        user = User.add(name=form.username.data, email=form.email.data, password=form.password.data)
-        send_verification_email(
-            user,
-            "Please verify your email address",
-            "email/user-email-address-verification.txt"
-        )
-        login_user(user)
-        flash.success("Account created. Please check your inbox to complete verification.")
-        return redirect(url_for("index.home"))
-
-    return render_template("users/signup.html", form=form)
+    return render_template("users/signup.html", props=json.dumps({
+        "recaptcha_site_key": current_app.config["RECAPTCHA_PUBLIC_KEY"],
+        "csrf_token": generate_csrf(),
+        "initial_form_data": form_data,
+        "initial_errors": form_errors
+    }))
 
 
 @users_bp.route("/login", methods=["GET", "POST"])
@@ -53,18 +62,25 @@ def login():
     if form.validate_on_submit():
         user = User.get(name=form.username.data)
         if user is None:
-            form.form_errors.append(f"Username {form.username.data} not found.")
-            return render_template("users/login.html", form=form)
-        
-        if not bcrypt.check_password_hash(user.password, form.password.data):
-            form.form_errors.append("Invalid username or password.")
-            return render_template("users/login.html", form=form)
+            form.username.errors.append(f"Username {form.username.data} not found.")
+        else:
+            if not bcrypt.check_password_hash(user.password, form.password.data):
+                form.password.errors.append("Invalid username or password.")
+            else:
+                login_user(user, remember=form.remember_me.data)
+                flash.success("Logged in successfully.")
+                return redirect(url_for("index.home"))
 
-        login_user(user, remember=form.remember_me.data)
-        flash.success("Logged in successfully.")
-        return redirect(url_for("index.home"))
+    form_errors = {k: ". ".join(v) for k, v in form.errors.items()}
+    form_data = dict(**form.data)
+    form_data.pop("csrf_token", None)
 
-    return render_template("users/login.html", form=form)
+    return render_template("users/login.html", props=json.dumps({
+        "recaptcha_site_key": current_app.config["RECAPTCHA_PUBLIC_KEY"],
+        "csrf_token": generate_csrf(),
+        "initial_form_data": form_data,
+        "initial_errors": form_errors
+    }))
 
 
 @users_bp.route("/verify-email")
@@ -129,14 +145,22 @@ def lost_username():
     if form.validate_on_submit():
         user = User.get(email=form.email.data)
         if user is None:
-            form.form_errors.append(f"The given email address ({form.email.data}) does not exist in our database.")
-            return render_template("users/lost_username.html", form=form)
+            form.email.errors.append(f"The given email address ({form.email.data}) does not exist in our database.")
+        else:
+            send_forgot_username_email(user)
+            flash.success("Username recovery link sent!")
+            return redirect(url_for("index.home"))
 
-        send_forgot_username_email(user)
-        flash.success("Username recovery link sent!")
-        return redirect(url_for("index.home"))
+    form_errors = {k: ". ".join(v) for k, v in form.errors.items()}
+    form_data = dict(**form.data)
+    form_data.pop("csrf_token", None)
 
-    return render_template("users/lost_username.html", form=form)
+    return render_template("users/lost-username.html", props=json.dumps({
+        "recaptcha_site_key": current_app.config["RECAPTCHA_PUBLIC_KEY"],
+        "csrf_token": generate_csrf(),
+        "initial_form_data": form_data,
+        "initial_errors": form_errors
+    }))
 
 
 @users_bp.route("/lost-password", methods=["GET", "POST"])
@@ -147,14 +171,22 @@ def lost_password():
     if form.validate_on_submit():
         user = User.get(name=form.username.data, email=form.email.data)
         if user is None:
-            form.form_errors.append(f"User with given username ({form.username.data}) and email ({form.email.data}) combination not found.")
-            return render_template("users/lost_password.html", form=form)
+            form.email.errors.append(f"User with given username ({form.username.data}) and email ({form.email.data}) combination not found.")
+        else:
+            send_forgot_password_email(user)
+            flash.success("Password reset link sent!")
+            return redirect(url_for("index.home"))
 
-        send_forgot_password_email(user)
-        flash.success("Password reset link sent!")
-        return redirect(url_for("index.home"))
+    form_errors = {k: ". ".join(v) for k, v in form.errors.items()}
+    form_data = dict(**form.data)
+    form_data.pop("csrf_token", None)
 
-    return render_template("users/lost_password.html", form=form)
+    return render_template("users/lost-password.html", props=json.dumps({
+        "recaptcha_site_key": current_app.config["RECAPTCHA_PUBLIC_KEY"],
+        "csrf_token": generate_csrf(),
+        "initial_form_data": form_data,
+        "initial_errors": form_errors
+    }))
 
 
 @users_bp.route("/reset-password", methods=["GET", "POST"])
@@ -188,7 +220,11 @@ def reset_password():
         flash.success("Password reset!")
         return redirect(url_for("index.home"))
 
-    return render_template("users/reset_password.html", form=form)
+    form_errors = {k: ". ".join(v) for k, v in form.errors.items()}
+    return render_template("users/reset-password.html", props=json.dumps({
+        "csrf_token": generate_csrf(),
+        "initial_errors": form_errors
+    }))
 
 
 @users_bp.route("/logout")
