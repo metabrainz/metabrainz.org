cf10335413a043559c988c05f1062de0c1daff8d
Kartik Ohri <kartikohri13@gmail.com>
2024-04-26 10:38:35 +0530

    add tests for access token and refresh token format

--- oauth/generator.py
+++ oauth/generator.py
@@ -2,8 +2,16 @@ from authlib.common.security import generate_token
 
 
 def create_access_token(*args, **kwargs):
-    return "mebo_" + generate_token(42)
+    return "meba_" + generate_token(42)
 
 
 def create_refresh_token(*args, **kwargs):
     return "mebr_" + generate_token(48)
+
+
+def create_client_secret(*args, **kwargs):
+    return "mebs_" + generate_token(48)
+
+
+def create_client_id(*args, **kwargs):
+    return generate_token(24)
