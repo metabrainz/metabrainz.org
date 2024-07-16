7bfc78977ee8af151e955e04f58291141a7f3338
Kartik Ohri <kartikohri13@gmail.com>
2024-07-16 19:31:56 +0530

    Fix currency for stripe payments

--- metabrainz/payments/forms.py
+++ metabrainz/payments/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from flask_babel import lazy_gettext
 from wtforms import StringField, BooleanField
 from wtforms.fields import RadioField, DecimalField, IntegerField
+from wtforms.fields.simple import HiddenField
 from wtforms.validators import DataRequired
 from metabrainz.payments import Currency
 
@@ -27,4 +28,5 @@ class DonationForm(BasePaymentForm):
 
 class PaymentForm(BasePaymentForm):
     """Payment form for organizations."""
+    currency = HiddenField()
     invoice_number = IntegerField(validators=[DataRequired(lazy_gettext("You need to specify invoice number!"))])
