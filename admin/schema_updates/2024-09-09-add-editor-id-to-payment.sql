dd8757cc19689b96f27eca5d650ef6882c03edbf
Kartik Ohri <kartikohri13@gmail.com>
2024-09-10 15:47:11 +0530

    fix tests and column update

--- admin/schema_updates/2024-09-09-add-editor-id-to-payment.sql
+++ admin/schema_updates/2024-09-09-add-editor-id-to-payment.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE supporter ADD COLUMN musicbrainz_row_id INTEGER;
-ALTER TABLE payment ADD COLUMN editor_id INTEGER;
+ALTER TABLE supporter ADD COLUMN musicbrainz_row_id INTEGER UNIQUE;
+ALTER TABLE payment ADD COLUMN editor_id INTEGER UNIQUE;
 
 COMMIT;
