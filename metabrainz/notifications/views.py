c77f153d713fcf54fab845e73c673513c9a4fd05
junaid <shaikjunaid124@gmail.com>
2025-05-28 17:57:44 +0000

    Change endpoints to take token from params, add API error handling (taken from listenbrainz errors.py), add limits to count and offset.

--- metabrainz/notifications/views.py
+++ metabrainz/notifications/views.py
@@ -1,20 +1,22 @@
-from flask import Blueprint, g, request, Response, jsonify
-from metabrainz.api.decorators import token_required
+from flask import Blueprint, request, jsonify
 from datetime import datetime, timezone
 from metabrainz.model.notification import NotificationProjectType
-from metabrainz.db.notification import fetch_notifications, validate_notification_ids_for_user, mark_read_unread, delete_notifications
-
+from metabrainz.db.notification import fetch_notifications, mark_read_unread, delete_notifications
+from metabrainz.errors import APIBadRequest
 
 DEFAULT_NOTIFICATION_FETCH_COUNT = 100
+MAX_ITEMS_PER_GET = 1000 # From listenbrainz.webserver.views.api_tools
+
+
 notification_bp = Blueprint("notification", __name__)
 
 
-@notification_bp.get("/fetch")
-@token_required
-def get_notifications():
+@notification_bp.get("/<int:user_id>/fetch")
+# TODO: Add authorization decorator.
+def get_notifications(user_id: int):
     """
     Fetch notifications for a user.
-    An access token (available on https://metabrainz.org/profile) must be provided in the Authorization header.
+    An access token must be provided as a query paramater.
     
     If none of the optional parameters are specified, this endpoint returns the 
     :data:`~DEFAULT_NOTIFICATION_FETCH_COUNT` most recent notifications across all projects, including both read and unread.
@@ -35,10 +37,10 @@ def get_notifications():
             }
         ]
 
-    :reqheader Authorization: Token <access token>
+    :param token: Required. Access token for authentication.
     :param project: Optional. Comma seperated lowercase list of MetaBrainz projects to filter by. If not specified, all projects are included.
-    :param count: Optional. Number of notifications to fetch. Defaults to :data:`~DEFAULT_NOTIFICATION_FETCH_COUNT`.
-    :param offset: Optional. Offset for pagination. Defaults to 0.
+    :param count: Optional. Number of notifications to fetch. Must be in the range of [1, :data:`~MAX_ITEMS_PER_GET`]. Defaults to :data:`~DEFAULT_NOTIFICATION_FETCH_COUNT`.
+    :param offset: Optional. Offset for pagination. Must be a non negative integer. Defaults to 0. 
     :param until_ts: Optional. Upper bound for the notification timestamp(as a UNIX epoch). Defaults to the current time.
     :param unread_only: Optional. If set to ``t``, only unread notifications are returned. If set to ``f`` or omitted, both unread and read notifications are returned.
     :statuscode 200: Notifications fetched successfully.
@@ -46,20 +48,22 @@ def get_notifications():
     :resheader Content-Type: *application/json*
 
     """
-    token = g._access_token
-    user_id = token.owner_id
-
+    
     count = request.args.get('count')
     if count is None:
         count = DEFAULT_NOTIFICATION_FETCH_COUNT
     else:
         count = int(count)
+        if not (count >= 1 and count <= MAX_ITEMS_PER_GET):
+            raise APIBadRequest('Invalid count')
 
     offset = request.args.get('offset')
     if offset is None:
         offset = 0
     else:
         offset = int(offset)
+        if offset < 0:
+            raise APIBadRequest('Invalid offset')
 
     until_ts = request.args.get('until_ts')
     if until_ts is None:
@@ -68,7 +72,7 @@ def get_notifications():
         try:
             until_ts = datetime.fromtimestamp(float(until_ts), tz=timezone.utc)
         except ValueError:
-            return Response('Invalid Until_Timestamp ', 400)
+            raise APIBadRequest('Invalid Until_Timestamp ')
 
     project = request.args.get('project')
     projects=[]
@@ -81,7 +85,7 @@ def get_notifications():
             try:
                 NotificationProjectType(p)
             except ValueError:
-                return Response(f'Invalid Project Type{p}\n', 400)
+                raise APIBadRequest(f'Invalid Project Type{p}\n')
 
     unread_only = request.args.get('unread_only')
     if unread_only is None or unread_only == 'f':
@@ -93,12 +97,12 @@ def get_notifications():
     return jsonify(data)
 
 
-@notification_bp.post("/mark-read")
-@token_required
-def mark_notifications():
+@notification_bp.post("<int:user_id>/mark-read")
+# TODO: Add authorization decorator.
+def mark_notifications(user_id: int):
     """
     Mark notifications as read or unread for a user.
-    An access token (available at https://metabrainz.org/profile) must be provided in the Authorization header.
+    An access token must be provided as a query paramater.
 
     The request must include a JSON body with at least one of the ``read`` or ``unread`` arrays containing notification ID's.
 
@@ -119,64 +123,50 @@ def mark_notifications():
             "status": "ok"
         }
     
-    :reqheader Authorization: Token <access token>
+    :param token: Required. Access token for authentication.
     :reqheader Content-Type: *application/json*
     :statuscode 200: Notifications successfully updated.
     :statuscode 400: Invalid input.
-    :statuscode 403: One or more IDs do not belong to the user.
-    :statuscode 500: Internal error, notifications could not be updated.
     :resheader Content-Type: *application/json*
+
     """
-    token = g._access_token
-    user_id = token.owner_id
 
     data = request.json
     if not data:
-        return Response('Invalid Input', 400)
+        raise APIBadRequest('Invalid Input')
     read = data.get('read', [])
     unread = data.get('unread', [])
 
     # Validate read/unread data
     if not read and not unread:
-        return Response('Missing Read and Unread IDs', 400)
+        raise APIBadRequest('Missing Read and Unread IDs')
     if read:
         if isinstance(read, list):
             if all(isinstance(i, int) for i in read):
                 pass
             else:
-                return Response('Read values must be Integers', 400)
+                raise APIBadRequest('Read values must be Integers')
         else:
-            return Response("'read' must be an Array", 400)
+            raise APIBadRequest("'read' must be an Array")
     if unread:
         if isinstance(unread, list):
             if all(isinstance(i, int) for i in unread):
                 pass
             else:
-                return Response('Unread values must be Integers', 400)
+                raise APIBadRequest('Unread values must be Integers')
         else:
-            return Response("'unread' must be an Array", 400)
+            raise APIBadRequest("'unread' must be an Array")
 
-    notification_ids = read+unread
-    if len(notification_ids) == 0:
-        return Response("Missing 'read' and 'unread' Array", 400)
+    mark_read_unread(user_id, tuple(read), tuple(unread))
+    return jsonify({'status': 'ok'}), 200
 
-    if not validate_notification_ids_for_user(user_id, tuple(notification_ids)):
-        return Response("Invalid ID/IDs provided", 403)
 
-    rows_affected = mark_read_unread(user_id, tuple(read), tuple(unread))
-
-    if rows_affected == len(notification_ids):
-        return jsonify({'status': 'ok'}), 200
-    else:
-        return jsonify({'status': 'error', 'message':'Notifications werent updated'}), 500
-
-    
-@notification_bp.post("/delete")
-@token_required
-def remove_notifications():
+@notification_bp.post("<int:user_id>/delete")
+# TODO: Add Authorization decorator.
+def remove_notifications(user_id: int):
     """
     Delete notifications for a user.
-    An access token (available at https://metabrainz.org/profile) must be provided in the Authorization header.
+    An access token must be provided as a query paramater.
 
     The request must include a JSON array of notification IDs that belongs to the user.
 
@@ -194,35 +184,27 @@ def remove_notifications():
             "status": "ok"
         }
 
-    :reqheader Authorization: Token <access token>
+    :param token: Required. Access token for authentication.
     :reqheader Content-Type: *application/json*
     :statuscode 200: Notifications successfully deleted.
     :statuscode 400: Invalid input.
-    :statuscode 403: One or more IDs do not belong to the user.
-    :statuscode 500: Internal error, notifications could not be deleted.
     :resheader Content-Type: *application/json*
 
     """
-    token = g._access_token
-    user_id = token.owner_id
 
     delete_ids = request.json
     if not delete_ids:
-        return Response('Missing notification IDs for deletion', 400)
+        raise APIBadRequest('Missing notification IDs for deletion')
+    
+    # Validate delete_ids array.
     if isinstance(delete_ids, list):
             if all(isinstance(j, int) for j in delete_ids):
                 pass
             else:
-                return Response('ID values must be Integers', 400)
+                raise APIBadRequest('ID values must be Integers')
     else:
-        return Response("ID's must be in an Array", 400)
-    
-    if not validate_notification_ids_for_user(user_id, tuple(delete_ids)):
-        return Response("Invalid ID/IDs provided", 403)
+        raise APIBadRequest("ID's must be in an Array")
 
-    res = delete_notifications(user_id, tuple(delete_ids))
-    if res:
-        return jsonify({'status': 'ok'}), 200
-    else:
-        return jsonify({'status': 'error', 'message':'Notifications werent deleted'}), 500
+    delete_notifications(user_id, tuple(delete_ids))
+    return jsonify({'status': 'ok'}), 200
     
