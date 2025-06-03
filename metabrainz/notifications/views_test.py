9484e77fcb554e0d02004da8e448ad2a574a9bc8
junaid <shaikjunaid124@gmail.com>
2025-06-03 07:33:32 +0000

    test return values of endpoints, fix minor bugs

--- metabrainz/notifications/views_test.py
+++ metabrainz/notifications/views_test.py
@@ -7,11 +7,14 @@ from metabrainz.notifications.views import DEFAULT_NOTIFICATION_FETCH_COUNT, MAX
 
 class NotificationViewsTest(FlaskTestCase):
     
-    @mock.patch('metabrainz.notifications.views.fetch_notifications', return_value=None)
+    @mock.patch('metabrainz.notifications.views.fetch_notifications')
     def test_get_notifications(self, mock_fetch):
         user_id = 1
         projects =  tuple(i.value for i in NotificationProjectType )
         until_ts = datetime.now(timezone.utc)
+        expected_value=[{'t_id':1}, {'t_id':2}]
+        mock_fetch.return_value=expected_value
+        
         # With no optional parameters.
         response = self.client.get(f'notification/{user_id}/fetch')
         self.assert200(response)
@@ -23,6 +26,7 @@ class NotificationViewsTest(FlaskTestCase):
             mock.ANY, # Unfortunately time flies away in between the call.
             False
         )
+        self.assertListEqual(response.json, expected_value)
         # With all optional paramters.
         url = f'notification/{user_id}/fetch?project=listenbrainz,metabrainz&count=3&offset=1&until_ts={until_ts.timestamp()}&unread_only=t'
         response = self.client.get(url)
@@ -35,12 +39,15 @@ class NotificationViewsTest(FlaskTestCase):
             until_ts,
             True
         )
+        self.assertListEqual(response.json, expected_value)
         # Bad requests.
-        bad_params=[('count',MAX_ITEMS_PER_GET+1), ('offset',-1),('until_ts','asdf'),('project','tidal,spotify'),
-                    ('unread_only','true')]
+        bad_params=[('count',MAX_ITEMS_PER_GET+1,'Invalid count'), ('offset',-1,'Invalid offset'),
+                    ('until_ts','asdf','Invalid Until_Timestamp'), ('project','tidal,spotify','Invalid project name'),
+                    ('unread_only','true','Invalid unread_only option')]
         for param in bad_params:
             res = self.client.get(f'notification/{user_id}/fetch?{param[0]}={param[1]}')
             self.assert400(res)
+            self.assertEqual(res.json['error'],param[2])
     
     @mock.patch('metabrainz.notifications.views.mark_read_unread', return_value=None)
     def test_mark_notifications(self, mock_mark):
@@ -58,6 +65,7 @@ class NotificationViewsTest(FlaskTestCase):
             tuple(read),
             tuple(unread)
         )
+        self.assertEqual(res.json['status'],'ok')
         # Only read array.
         res = self.client.post(
             f'notification/{user_id}/mark-read',
@@ -69,19 +77,24 @@ class NotificationViewsTest(FlaskTestCase):
             tuple(read),
             tuple([])
         )
+        self.assertEqual(res.json['status'],'ok')
         # Bad requests.
         res = self.client.post(
             f'notification/{user_id}/mark-read',
             json={}
             )
         self.assert400(res)
-        bad_data=[([],[]),(1,[2]),([4],['1st','b'])]
+        bad_data=[([],[],'Missing Read and Unread IDs'),
+                  (1,[2],"'read' must be an Array" ),
+                  ([4],['1st','b'],'Unread values must be Integers')
+                ]
         for d in bad_data:
             res = self.client.post(
             f'notification/{user_id}/mark-read',
             json={"read": d[0], "unread":d[1]}
             )
             self.assert400(res)
+            self.assertEqual(res.json['error'],d[2])
         # Database error.
         mock_mark.side_effect= Exception()
         res = self.client.post(
@@ -89,6 +102,7 @@ class NotificationViewsTest(FlaskTestCase):
             json={"read": read, "unread": unread}
             )
         self.assert503(res)
+        self.assertEqual(res.json['error'], 'Cannot update read values right now.')
     
     @mock.patch('metabrainz.notifications.views.delete_notifications', return_value=None)
     def test_remove_notifications(self, mock_delete):
@@ -103,13 +117,23 @@ class NotificationViewsTest(FlaskTestCase):
             user_id,
             tuple(delete),
         )
+        self.assertEqual(res.json['status'],'ok')
         # Bad requests.
-        bad_data = [1,['1'], []]
+        bad_data = [(1,"ID's must be in an Array"),(['1'],'ID values must be Integers'),
+                    ([],'Missing notification IDs for deletion')]
         for d in bad_data:
             res = self.client.post(
-            f'notification/{user_id}/delete',
-            json=d
+                f'notification/{user_id}/delete',
+                json=d
             )
             self.assert400(res)
+        # Database error.
+        mock_delete.side_effect= Exception()
+        res = self.client.post(
+            f'notification/{user_id}/delete',
+            json=[i for i in delete]
+            )
+        self.assert503(res)
+        self.assertEqual(res.json['error'], 'Cannot delete notifications right now.')
 
 
