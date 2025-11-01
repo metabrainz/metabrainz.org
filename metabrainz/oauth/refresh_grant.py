a3e1f084527bba733cdda48d98bed51a8741353a
Kartik Ohri <kartikohri13@gmail.com>
2025-11-01 23:19:21 +0530

    migrate oauth module to `metabrainz` package and remove obsolete files
    Since, we no longer need to run two separate containers for OAuth and main
    website merge both modules back.

---
+++ metabrainz/oauth/refresh_grant.py
@@ -0,0 +1,48 @@
+from authlib.oauth2.rfc6749 import grants, InvalidRequestError
+
+from metabrainz.model.user import User
+from metabrainz.model import db, OAuth2RefreshToken, OAuth2AccessToken
+
+
+class RefreshTokenGrant(grants.RefreshTokenGrant):
+
+    TOKEN_ENDPOINT_AUTH_METHODS = ["client_secret_basic", "client_secret_post"]
+    INCLUDE_NEW_REFRESH_TOKEN = True
+
+    def authenticate_refresh_token(self, refresh_token):
+        token = db.session\
+            .query(OAuth2RefreshToken)\
+            .filter_by(refresh_token=refresh_token)\
+            .first()
+        # revoked token used, revoke other tokens for same client and user
+        # https://datatracker.ietf.org/doc/html/rfc6819#section-5.2.2.3
+        if token and token.revoked:
+            db.session.query(OAuth2AccessToken).filter_by(
+                client_id=token.client_id,
+                user_id=token.user_id
+            ).update({OAuth2AccessToken.revoked: True})
+            db.session.query(OAuth2RefreshToken).filter_by(
+                client_id=token.client_id,
+                user_id=token.user_id
+            ).update({OAuth2RefreshToken.revoked: True})
+            db.session.commit()
+            raise InvalidRequestError("\"refresh_token\" in request was already revoked.")
+        return token
+
+    def authenticate_user(self, credential):
+        return User.get(id=credential.user_id)
+
+    def revoke_old_credential(self, credential):
+        old_access_tokens = db.session\
+            .query(OAuth2AccessToken)\
+            .filter_by(user_id=credential.user_id, client_id=credential.client_id, revoked=False)\
+            .order_by(OAuth2AccessToken.issued_at)\
+            .all()
+
+        if len(old_access_tokens) > 0:
+            old_access_tokens.pop()  # do not revoke the latest issued token
+            for token in old_access_tokens:
+                token.revoked = True
+
+        credential.revoked = True
+        db.session.commit()
---
+++ metabrainz/oauth/revocation.py
@@ -0,0 +1,36 @@
+from authlib.oauth2.rfc7009 import RevocationEndpoint
+
+from metabrainz.model import db, OAuth2AccessToken, OAuth2RefreshToken
+
+
+class OAuth2RevocationEndpoint(RevocationEndpoint):
+
+    CLIENT_AUTH_METHODS = ["client_secret_basic", "client_secret_post"]
+
+    def query_token(self, token_str, token_type_hint):
+        if token_type_hint == "access_token":
+            token = db.session.query(OAuth2AccessToken).filter_by(access_token=token_str).first()
+        elif token_type_hint == "refresh_token":
+            token = db.session.query(OAuth2RefreshToken).filter_by(refresh_token=token_str).first()
+        else:  # without token_type_hint
+            token = db.session.query(OAuth2AccessToken).filter_by(access_token=token_str).first()
+            if not token:
+                token = db.session.query(OAuth2RefreshToken).filter_by(refresh_token=token_str).first()
+        return token
+
+    def revoke_token(self, token, request):
+        if isinstance(token, OAuth2RefreshToken):
+            # if the token is a refresh_token, revoke all associated access_tokens as well
+            # in accordance with rfc 7009
+            access_tokens = db.session.query(OAuth2AccessToken).filter_by(
+                client_id=token.client_id,
+                user_id=token.user_id,
+                revoked=False
+            ).all()
+            for access_token in access_tokens:
+                access_token.revoked = True
+            db.session.add_all(access_tokens)
+
+        token.revoked = True
+        db.session.add(token)
+        db.session.commit()
