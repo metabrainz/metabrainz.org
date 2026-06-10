b6510fc50c5ef575a8a9a4a03791cb13621bd474
Kartik Ohri <kartikohri13@gmail.com>
2026-06-10 14:47:31 +0200

    Develop user supporter schema migrations

--- admin/schema_updates/2026-04-16-edit-username.sql
+++ admin/schema_updates/2026-04-16-edit-username.sql
@@ -4,12 +4,12 @@ ALTER TYPE moderation_action_type ADD VALUE 'edit_username';
 BEGIN;
 
 CREATE TABLE old_username (
-    id          INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id          INTEGER GENERATED ALWAYS AS IDENTITY,
     username    TEXT NOT NULL,
-    deleted_at  TIMESTAMP NOT NULL DEFAULT NOW()
+    deleted_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
+ALTER TABLE old_username ADD CONSTRAINT old_username_pkey PRIMARY KEY (id);
 CREATE INDEX old_username_username_idx ON old_username (username);
-CREATE UNIQUE INDEX user_name_unq_idx ON "user" (name);
 
 COMMIT;
